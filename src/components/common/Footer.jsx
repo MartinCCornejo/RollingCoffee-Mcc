7b8fe3687b18649ae39000c364317faba37faa5c
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-05 12:50:34 -0300

    Agregamos la estetica del navbar y las opciones del menu

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="text-center py-3 bg-mainColor text-light">
+            <p>&copy; Martín Cornejo - Todos los derechos reservados</p>
+        </footer>
     );
 };
 
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,8 +1,24 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from '../../assets/Coffee_Logo.png'
+
 const Menu = () => {
     return (
-        <div>
-            
-        </div>
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="Logo de RollingCOffee" className="img-fluid" width={170}/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#home">Administrador</Nav.Link>
+            <Nav.Link href="#home">Registro</Nav.Link>
+            <Nav.Link href="#link">Iniciar sesión</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 };
 
