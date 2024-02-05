9f78e99f9aba9d23f0718bb4f1f0b8e9caea99ad
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-05 13:52:57 -0300

    Colocamos la imagen del banner

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,8 +1,10 @@
+import imgBanner from '../../assets/banner.jpg'
 
 const Inicio = () => {
     return (
         <section className="flex-grow-1">
             {/* Aqui va el main de index  */}
+            <img src={imgBanner} alt="Imagen banner de la pagina" className='img-banner'/>
         </section>
     );
 };
