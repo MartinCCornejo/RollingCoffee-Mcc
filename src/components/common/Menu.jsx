83d97a54510066a648d99632d49fafd3c7c8219e
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-07 16:56:07 -0300

    Agregamos las rutas correspondientes en el componente Menu

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,20 +1,21 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
 import logo from '../../assets/Coffee_Logo.png'
 
 const Menu = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary navbar">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand as={Link} to={'/'}>
           <img src={logo} alt="Logo de RollingCOffee" className="img-fluid" width={160}/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto fw-medium text-montserrat">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#home">Administrador</Nav.Link>
-            <Nav.Link href="#home">Registro</Nav.Link>
-            <Nav.Link href="#home">Iniciar sesión</Nav.Link>
+            <NavLink end to={'/'} className='nav-link'>Inicio</NavLink>
+            <NavLink end to={'/administrador'} className='nav-link'>Administrador</NavLink>
+            <NavLink end to={'/registro'} className='nav-link'>Registro</NavLink>
+            <NavLink end to={'/login'} className='nav-link'>Login</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
