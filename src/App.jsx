cb58b489d9261a615f3ef4016ed9ac9b5a6ab12e
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-07 16:33:32 -0300

    Agregamos rutas en el componente app

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import Footer from "./components/common/Footer";
 import Menu from "./components/common/Menu";
 import Inicio from "./components/pages/Inicio";
+import Administrador from "./components/pages/Administrador";
+import Error404 from "./components/pages/error404";
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import "./App.css";
 
 
 function App() {
   return (
-    <>
+    // Administrador de rutas 
+    <BrowserRouter>
       <Menu></Menu>
-      <Inicio></Inicio>
+      <Routes>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
+        <Route path="*" element={<Error404></Error404>}></Route>
+      </Routes>
       <Footer></Footer>
-    </>
+    </BrowserRouter>
   );
 }
 
