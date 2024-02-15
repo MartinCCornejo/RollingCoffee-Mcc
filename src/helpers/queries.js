0242e573dcaf89ee92176052d2599bf955cb39f6
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-15 10:47:40 -0300

    Creamos la funcion para crear un producto y enviarlo como metodo PHOST a la API

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,3 +1,25 @@
 const URI_Producto = import.meta.env.VITE_API_PRODUCTO;
 
-console.log(URI_Producto)
+console.log(URI_Producto);
+
+// POST
+async function crearProductoAPI (productoNuevo) {
+    try {
+
+        const respuesta = await fetch(URI_Producto, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(productoNuevo)
+        });
+        // const data = await respuesta.json();
+        console.log(respuesta);
+        return respuesta;
+
+    } catch (error) {
+
+        console.log(error)
+        
+    }
+}
