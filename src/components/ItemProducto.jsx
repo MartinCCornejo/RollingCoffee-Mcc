0882234ad95592f2ac5d67d5aab538d165038dfc
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-22 10:12:52 -0300

    Agregamos la logica correspondiente para borrar un producto mediante su id

--- src/components/ItemProducto.jsx
+++ src/components/ItemProducto.jsx
@@ -1,9 +1,10 @@
 import { Button } from "react-bootstrap";
 import Swal from "sweetalert2";
+import { borrarProductoAPI } from "../helpers/queries";
 
-const ItemProducto = ({producto}) => {
+const ItemProducto = ({ producto }) => {
 
-  const borrarProducto = ()=> {
+  const borrarProducto = () => {
     Swal.fire({
       title: "Estas seguro de borrar este producto?",
       text: "No podras revertir esta acción!",
@@ -12,43 +13,51 @@ const ItemProducto = ({producto}) => {
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
       confirmButtonText: "Borrar",
-      cancelButtonText: "Cancelar"
-    }).then((result) => {
+      cancelButtonText: "Cancelar",
+    }).then(async (result) => {
       if (result.isConfirmed) {
-        Swal.fire({
-          title: "Producto eliminado",
-          text: `El producto '${producto.nombreProducto}' fue eliminado correctamente`,
-          icon: "success"
-        });
+        const respuesta = await borrarProductoAPI(producto.id);
+        if (respuesta.status === 200) {
+          Swal.fire({
+            title: "Producto eliminado",
+            text: `El producto '${producto.nombreProducto}' fue eliminado correctamente`,
+            icon: "success",
+          });
+        } else {
+          Swal.fire({
+            title: "Ocurrio un error",
+            text: `El producto '${producto.nombreProducto}' NO fue eliminado, intente esta operación en unos minutos`,
+            icon: "error",
+          });
+        }
       }
     });
-  }
+  };
 
-
-    return (
-        <tr>
-            <td className="text-center">{producto.id}</td>
-            <td>{producto.nombreProducto}</td>
-            <td className="text-center">{producto.precio}</td>
-            <td className="text-center">
-              <img
-                src={producto.imagen}
-                alt="Imagen del producto"
-                className="img-fluid"
-                width={130}
-              />
-            </td>
-            <td>{producto.categoria}</td>
-            <td className="text-center">
-              <Button variant="warning">
-                <i className="bi bi-pencil-square"></i>
-              </Button>
-              <Button variant="danger" className="ms-2" onClick={borrarProducto}>
-                <i className="bi bi-trash"></i>
-              </Button>
-            </td>
-          </tr>
-    );
+  return (
+    <tr>
+      <td className="text-center">{producto.id}</td>
+      <td>{producto.nombreProducto}</td>
+      <td className="text-center">{producto.precio}</td>
+      <td className="text-center">
+        <img
+          src={producto.imagen}
+          alt="Imagen del producto"
+          className="img-fluid"
+          width={130}
+        />
+      </td>
+      <td>{producto.categoria}</td>
+      <td className="text-center">
+        <Button variant="warning">
+          <i className="bi bi-pencil-square"></i>
+        </Button>
+        <Button variant="danger" className="ms-2" onClick={borrarProducto}>
+          <i className="bi bi-trash"></i>
+        </Button>
+      </td>
+    </tr>
+  );
 };
 
 export default ItemProducto;
--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -3,31 +3,44 @@ const URI_Producto = import.meta.env.VITE_API_PRODUCTO;
 console.log(URI_Producto);
 
 // POST
-export async function crearProductoAPI (productoNuevo) {
-    try {
-        const respuesta = await fetch(URI_Producto, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(productoNuevo)
-        });
-        // const data = await respuesta.json();
-        return respuesta;
-
-    } catch (error) {
-        console.log(error);
-    }
+export async function crearProductoAPI(productoNuevo) {
+  try {
+    const respuesta = await fetch(URI_Producto, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productoNuevo),
+    });
+    // const data = await respuesta.json();
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-// GET 
+// GET
 
-export async function leerProductosAPI () {
-    try {
-        const respuesta = await fetch(URI_Producto);
-        return respuesta;
-        // return respuesta;
-    } catch (error) {
-        console.log(error);
-    }
+export async function leerProductosAPI() {
+  try {
+    const respuesta = await fetch(URI_Producto);
+    return respuesta;
+    // return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// DELETE
+
+export async function borrarProductoAPI(id) {
+  try {
+    const respuesta = await fetch(`${URI_Producto}/${id}`, {
+      method: "DELETE"
+    });
+    console.log(respuesta);
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
 }
