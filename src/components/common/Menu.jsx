7d1186bdfdc8a78f9c92ed8f0d33221a25552a6d
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-03-11 17:53:08 -0300

    Mostramos las opciones del menu dependiendo si el usuario esta logeado o no

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,45 +1,77 @@
 import { useState, useEffect } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link, NavLink } from "react-router-dom";
-import logo from '../../assets/Coffee_Logo.png'
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import logo from "../../assets/Coffee_Logo.png";
 
-const Menu = () => {
-    const [scrolled, setScrolled] = useState(false);
+const Menu = ({ usuarioLogueado, setUsuarioLogeado }) => {
+  const [scrolled, setScrolled] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 0) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        };
+  const navegacion = useNavigate();
 
-        window.addEventListener('scroll', handleScroll);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+    window.addEventListener("scroll", handleScroll);
 
-    return (
-        <Navbar expand="lg" className={`bg-body-tertiary navbar ${scrolled ? 'box-shadow' : ''}`}>
-            <Container>
-                <Navbar.Brand as={Link} to={'/'}>
-                    <img src={logo} alt="Logo de RollingCOffee" className="img-fluid" width={160}/>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto fw-medium text-montserrat">
-                        <NavLink end to={'/'} className='nav-link'>Inicio</NavLink>
-                        <NavLink end to={'/administrador'} className='nav-link'>Administrador</NavLink>
-                        <NavLink end to={'/registro'} className='nav-link'>Registro</NavLink>
-                        <NavLink end to={'/login'} className='nav-link'>Login</NavLink>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const logout = () => {
+    sessionStorage.removeItem('loginRollingCoffee');
+    setUsuarioLogeado('');
+    navegacion('/');
+  }
+
+  return (
+    <Navbar
+      expand="lg"
+      className={`bg-body-tertiary navbar ${scrolled ? "box-shadow" : ""}`}
+    >
+      <Container>
+        <Navbar.Brand as={Link} to={"/"}>
+          <img
+            src={logo}
+            alt="Logo de RollingCOffee"
+            className="img-fluid"
+            width={160}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto fw-medium text-montserrat">
+            <NavLink end to={"/"} className="nav-link">
+              Inicio
+            </NavLink>
+            <NavLink end to={"/registro"} className="nav-link">
+              Registro
+            </NavLink>
+            {usuarioLogueado.length > 0 ? (
+              <>
+                <NavLink end to={"/administrador"} className="nav-link">
+                  Administrador
+                </NavLink>
+                <Button variant="danger" onClick={logout}>Logout</Button>
+              </>
+            ) : (
+              <>
+                <NavLink end to={"/login"} className="nav-link">
+                  Login
+                </NavLink>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Menu;
